04620d479aa910129286d33c0fd4546e34d7a65c
bastiansepulveda <bastian.sepulveda@sansano.usm.cl>
2021-07-22 23:15:45 -0400

    Se borra la contraseña en el archivo db_config.php

--- plantilla/db_config.php
+++ plantilla/db_config.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Detalles de la conexión */
-$conn_string = "host=localhost port=5432 dbname=Tarea1 user=postgres password=ipe1997alc";
+$conn_string = "host=localhost port=5432 dbname=Tarea1 user=postgres password=<contraseña>";
 // Recuerde reemplazar "<contraseña>" por su contraseña y "<nombre_db>" por el nombre de su BD. No se incluyen los "<>".
 // Establecemos una conexión con el servidor postgresSQL
 $dbconn = pg_connect($conn_string);
